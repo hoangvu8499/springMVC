ae6cea5c8c3e10f541957be6c0593b90a719d268
hoangvu <hoangvu8499@gmail.com>
2021-02-18 10:04:29 +0700

    setup_security

--- src/main/java/com/springMVC/entity/BaseEntity.java
+++ src/main/java/com/springMVC/entity/BaseEntity.java
@@ -1,13 +1,19 @@
 package com.springMVC.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import javax.persistence.*;
-import java.util.Date;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
